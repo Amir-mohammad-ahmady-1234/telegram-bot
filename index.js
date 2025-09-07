abe00b5f9b6ed3e83b9cfd717a15db34285f1265
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-09-06 20:29:12 -0700

    add interduce

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const { Telegraf } = require("telegraf");
 
-// ⚡️ توکن باتت رو از BotFather بگیر و اینجا بذار
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// دستورات ساده
-bot.start((ctx) => ctx.reply("سلام سلطان 👑 باتت روشن شد 🚀"));
-bot.help((ctx) => ctx.reply("دستور /start رو بزن تا شروع کنیم"));
-bot.on("text", (ctx) => ctx.reply(`تو گفتی: ${ctx.message.text}`));
+bot.start((ctx) =>
+  ctx.reply(
+    "😁سلام! این بات برای نمایش رزومه امیرمحمد ساخته شده. با انتخاب یکی از دکمه ها ادامه دهید."
+  )
+);
+bot.help((ctx) => ctx.reply("برای پیدا معرفی امیرمحمد /interduce بزن"));
+bot.interduce((ctx) =>
+  ctx.reply(
+    `امیر محمد برنامه نویس فرانت اند هست یا 2 سال تجربه در این حوزه و مسلط به : 
+    html,css,tailsind, jsvascript, typescript,redux,zustand,reactQuery,react,nextjs.
+    گیت هاب : https://github.com/Amir-mohammad-ahmady-1234 👽`
+  )
+);
 
-// اجرا
+bot.on("text", (ctx) => ctx.reply(`این جزو دستورات بات نیست😑`));
+
 bot.launch();
